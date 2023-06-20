dbc4cc28aac3ed5ed209f546c612872d75e28ab4
Jay Long <jaylong255@gmail.com>
2023-06-20 09:11:52 -0500

    pointing the dns record at the cdn

--- terraform/modules/static-frontend/dns.tf
+++ terraform/modules/static-frontend/dns.tf
@@ -1,9 +1,11 @@
-// route53 dns record
 resource "aws_route53_record" "dns" {
   depends_on = [aws_cloudfront_distribution.distribution]
-  zone_id = var.hosted_zone_id
-  name    = "${var.environment}.${var.domain_name}"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_cloudfront_distribution.distribution.domain_name}"]
+  zone_id    = var.hosted_zone_id
+  name       = "${var.environment}.${var.domain_name}"
+  type       = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
