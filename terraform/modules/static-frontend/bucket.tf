a032407538150143019850eea1c367049e774fb0
Jay Long <jaylong255@gmail.com>
2023-06-20 11:36:38 -0500

    adding static site config. removing acl

--- terraform/modules/static-frontend/bucket.tf
+++ terraform/modules/static-frontend/bucket.tf
@@ -1,5 +1,7 @@
 locals {
   // Generate a unique string for the bucket name.
+  // It's possible that this is unnecessary because of the `bucket_prefix` attribute, 
+  // but I'm keeping it in case I need to be able to predict the bucket name somewhere else.
   unique_string = substr(replace(sha1("${var.project}-${var.environment}-assets"), "/[^a-z0-9]/", ""), 0, 8) // substring of the hash of the primary segment of the bucket name
 }
 
@@ -7,15 +9,14 @@ locals {
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.project}-${var.environment}-assets-${local.unique_string}"
 
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+
   tags = {
     Project     = var.project
     Environment = var.environment
     Terraform   = "true"
   }
 }
-
-# # ACL for the bucket.
-# resource "aws_s3_bucket_acl" "bucket" {
-#   bucket = "${aws_s3_bucket.bucket.bucket}"
-#   acl    = "public-read"
-# }
