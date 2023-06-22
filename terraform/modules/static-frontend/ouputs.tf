907ef61826a1b84f52b6757177182ba791100c39
Jay Long <jaylong255@gmail.com>
2023-06-21 20:20:59 -0500

    removing un-needed outputs

--- terraform/modules/static-frontend/ouputs.tf
+++
@@ -1,7 +0,0 @@
-output "origin_access_identity_iam_arn" {
-  value = "${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"
-}
-
-output "origin_access_identity_iam_arn_id" {
-  value = "${aws_cloudfront_origin_access_identity.origin_access_identity.id}"
-}
