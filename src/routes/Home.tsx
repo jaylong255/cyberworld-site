bed226be15bc05abf5351eeac3fe4d5880ce273b
Jay Long <jaylong255@gmail.com>
2023-06-25 13:43:05 -0500

    deleting unused and commented code

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -10,11 +10,8 @@ function Home() {
   return (
     <Box // outermost container
       sx={{
-        // background color not quite black but almost
         background: "#181818",
         width: "100%",
-        // minHeight: "100vh",
-        // border: "2px solid blue",
       }}
     >
       <Box // horizontal alignment container
@@ -22,14 +19,10 @@ function Home() {
           margin: "none",
           padding: "none",
           border: "none",
-          // background: "red",
-          // border: "2px solid yellow",
           display: "flex",
           flexDirection: "row",
           justifyContent: "center",
-          // height: "100%",
           width: "100%",
-          // minHeight: "100vh",
         }}
       >
         <Box // vertical alignment container
@@ -51,56 +44,10 @@ function Home() {
             <CyberWorldLogo />
           </Box>
 
-          {/* <Typography
-            sx={{
-              color: "#A6E102",
-              fontFamily: "Ubuntu",
-              fontSize: "2rem",
-              textAlign: "center",
-              margin: "0px 0px 0px 0px",
-              padding: "0px 0px 0px 0px",
-            }}
-          >
-            _Builders
-          </Typography>
-
-          <Typography
-            sx={{
-              fontFamily: "Ubuntu",
-              fontSize: "2rem",
-              textAlign: "center",
-              margin: "0px 0px 0px 0px",
-              padding: "0px 0px 0px 0px",
-              color: "#1E1E1E",
-              mixBlendMode: "screen",
-              border: "1px solid #000000",
-              textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)"
-            }}
-          >
-            _Prophets
-          </Typography>
-
-          <Typography
-            sx={{
-              fontFamily: "Ubuntu",
-              fontSize: "2rem",
-              textAlign: "center",
-              margin: "0px 0px 0px 0px",
-              padding: "0px 0px 0px 0px",
-              color: "#1E1E1E",
-              mixBlendMode: "screen",
-              border: "1px solid #000000",
-              textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)"
-            }}
-          >
-            _Soldiers
-          </Typography> */}
-
           <Box // social link container
             sx={{
               margin: "32px 0px 0px 0px",
               padding: "none",
-              // border: "2px solid yellow",
               background: "#181818",
               display: "flex",
               flexDirection: "row",
@@ -158,7 +105,6 @@ function Home() {
             sx={{
               margin: "32px 8px 0px 0px",
               padding: "none",
-              // border: "2px solid yellow",
               background: "#181818",
               display: "flex",
               flexDirection: "column",
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
 
 function Root() {
   return (
     <div>
-      {/* <Header /> */}
       <Outlet />
     </div>
   );
