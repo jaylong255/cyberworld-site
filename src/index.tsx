de4535b7039a9d8ac97e6ed153793eea625cf587
Jay <jaylong255@gmail.com>
2023-10-26 15:36:23 +0000

    removing old junk

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "components/App";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
-import { worker } from "mocks/browser";
 import theme from "./theme";
 import "@fontsource/metropolis";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
@@ -10,13 +9,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import React from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-
-if (
-  process.env.NODE_ENV === "development" &&
-  import.meta.env.VITE_BACKEND === "mock"
-) {
-  worker.start();
-}
 
 const queryClient = new QueryClient({
   defaultOptions: {
