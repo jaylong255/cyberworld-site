ea9e05b7e9db58baa95727b99e7919417e276d87
Jay <jaylong255@gmail.com>
2023-10-26 15:42:36 +0000

    fixing src attribute on x logo

--- src/components/icons/TwitterLogo.tsx
+++ src/components/icons/TwitterLogo.tsx
@@ -1,7 +1,9 @@
+import src from "../../assets/images/x-green-small.png";
+
 function TwitterLogo() {
   return (
     <div>
-      <img src="src/assets/images/x-green-small.png" />
+      <img src={src} alt="X Logo" />
     </div>
   );
 }
